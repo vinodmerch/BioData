97a21c33f3cae33ddbe9a4c4bfca73066b9d8fcd
Vinod Merchant <7456068+vinodmerch@users.noreply.github.com>
2025-09-29 17:53:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleDetails() {
   const more = document.getElementById("more");
   more.style.display = more.style.display === "none" ? "block" : "none";
 }
+
+function toggleSection(id) {
+  const section = document.getElementById(id);
+  section.classList.toggle("hidden");
+}
